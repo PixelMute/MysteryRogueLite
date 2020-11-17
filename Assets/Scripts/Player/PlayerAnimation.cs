9f2ba3344feb85c03acf000e1e11f7ae9edae619
zamsler98 <zamsler98@gmail.com>
2020-11-17 15:32:29 -0600

    Add idle and walking animations

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator Animator;
+
+    public void StartWalkingAnimation()
+    {
+        Animator.Play("PlayerWalking");
+    }
+
+    public void StartIdleAnimation()
+    {
+        Animator.Play("PlayerIdle");
+    }
+}
