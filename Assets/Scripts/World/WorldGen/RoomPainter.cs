e68b1791e558ae0a3f6ba6ee143ebd01810e9d4c
zamsler98 <zamsler98@gmail.com>
2020-11-23 00:43:28 -0600

    Add abstract

--- Assets/Scripts/World/WorldGen/RoomPainter.cs
+++ Assets/Scripts/World/WorldGen/RoomPainter.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine.Tilemaps;
 
-namespace Assets.Scripts.World.WorldGen
+public abstract class RoomPainter
 {
-    class RoomPainter
+    public Room Room { get; set; }
+
+    public abstract void PaintRoom(Tilemap terrain);
+
+    protected void PaintBottomRow(Tilemap terrain)
     {
+
     }
 }
+
+public class EmptyRoomPainter : RoomPainter
+{
+    public override void PaintRoom(Tilemap terrain)
+    {
+
+    }
+}
+
